3ebd59411237c19d7965a1410264cb21f009a134
Leonardo Arrighi <Leonardoarrighi43@gmail.com>
2020-04-23 15:53:25 +0200

    Add ex3 LAB
    Works, but must be improved. Why ggscatter doesn't wok :(

---
+++ Lab/Lab_Ex3.R
@@ -0,0 +1,28 @@
+library("magrittr")
+library("ggplot2")
+library("ggpubr")
+# Dataframe
+Owners <- c( "Katy Perry", "Justin Bieber", "Taylor Swift", "Cristiano Ronaldo",
+             "Kim Kardashian", "Ariana Grande", "Selena Gomez", "Demi Lovato")
+Instagram <- c( 69, 98,107, 123, 110, 118, 135, 67)
+Twitter <- c( 109, 106, 86, 72, 59, 57, 56, 56)
+plot( Instagram, Twitter, pch=21, bg=2, xlim=c(60, 150), ylim=c(40, 120) )
+text( Instagram[-6], Twitter[-6]+5, Owners[-6], cex=0.8 )
+text( Instagram[6], Twitter[6]-5, Owners[6], cex=0.8 )
+
+# Create a more suitable dataframe
+all <- t(matrix(c( Instagram,
+                   Twitter), ncol = 2, nrow = length(Owners), ))
+colnames(all) <- c(Owners)
+rownames(all) <- c("Instagram", "Twitter")
+
+ggscatter(all, x = "Instagram", y = "Twitter", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Instagram", ylab = "Twitter")
+
+# Pearson test
+pearson <- cor.test(Instagram, Twitter, 
+                method = "pearson")
+pearson
+
