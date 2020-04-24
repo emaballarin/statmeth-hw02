f41143dba2a0346c31abe639259025d130c56cbf
Leonardo Arrighi <Leonardoarrighi43@gmail.com>
2020-04-24 17:34:50 +0200

    ex3_Lab is super good (with a nice plot ^^ )

--- Lab/Lab_Ex3.R
+++ Lab/Lab_Ex3.R
@@ -1,28 +1,32 @@
 library("magrittr")
 library("ggplot2")
 library("ggpubr")
+
 # Dataframe
 Owners <- c( "Katy Perry", "Justin Bieber", "Taylor Swift", "Cristiano Ronaldo",
              "Kim Kardashian", "Ariana Grande", "Selena Gomez", "Demi Lovato")
-Instagram <- c( 69, 98,107, 123, 110, 118, 135, 67)
+Instagram <- c( 69, 98, 107, 123, 110, 118, 135, 67)
 Twitter <- c( 109, 106, 86, 72, 59, 57, 56, 56)
+
+# Plot
 plot( Instagram, Twitter, pch=21, bg=2, xlim=c(60, 150), ylim=c(40, 120) )
 text( Instagram[-6], Twitter[-6]+5, Owners[-6], cex=0.8 )
 text( Instagram[6], Twitter[6]-5, Owners[6], cex=0.8 )
 
 # Create a more suitable dataframe
-all <- t(matrix(c( Instagram,
-                   Twitter), ncol = 2, nrow = length(Owners), ))
-colnames(all) <- c(Owners)
-rownames(all) <- c("Instagram", "Twitter")
+all <- data.frame(Owner = Owners, Instagram = Instagram, Twitter = Twitter)
 
-ggscatter(all, x = "Instagram", y = "Twitter", 
-          add = "reg.line", conf.int = TRUE, 
+# Plot correlation using ggpubr library
+ggscatter(all, x = 'Instagram', y = 'Twitter', 
+          color = 'red',   # for the points
+          add = "reg.line", 
+          add.params = list(color = "blue", fill = "lightgray"),  # for the line
+          conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Instagram", ylab = "Twitter")
+          xlab = "Instagram", ylab = "Twitter",
+          xlim=c(60, 150), ylim=c(40, 120))
 
 # Pearson test
 pearson <- cor.test(Instagram, Twitter, 
                 method = "pearson")
-pearson
 
