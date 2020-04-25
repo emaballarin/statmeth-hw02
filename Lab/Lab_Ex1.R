13daba78513b1a928271de0514add3efb72a4e94
Emanuele Ballarin <emanuele@ballarin.cc>
2020-04-25 18:10:20 +0200

    Review: LAB_01

--- Lab/Lab_Ex1.R
+++ Lab/Lab_Ex1.R
@@ -1,26 +1,46 @@
-library("ggplot2")
+# REQUIRED FUNCTIONS #
 
-set.seed(140898)
-
-R <- 60000
-n <- 10
-sigma <- 1
-
-samples <- array(0, c(1, R, n))
-for (i in 1:R){
-  samples[1, i, ] <- rnorm(n, 0, 1)
+# Biased sample-variance, done in an efficient manner
+var_b <- function(x)
+{
+    var(x)*(length(x)-1)/length(x)
 }
-samples_stat <- array(0, c(1, 2, R))
-samples_stat[1, 1, ] <- apply(samples[1, , ], 1, mean)
-samples_stat[1, 2, ] <- apply(samples[1, , ], 1, var)
 
-par (mfrow=c(1,1), oma=c(0,0,0,0))
 
-hist(samples_stat[1, 2, ], breaks= 40, probability = TRUE, 
-     xlab=expression(s^2), main= bquote(s^2), cex.main=1.5, ylim = c(0, 1.5))
-# s^2
-curve(((n-1)/sigma^2) * dchisq(x * ((n-1)/sigma^2), df = n - 1),
-      add = TRUE, col="red", lwd=2, main="N(0,1)")
-# sb^2    
+
+# EXERCISE #
+
+set.seed(123)    # Be reproducible
+
+R <- 1000        # Replications
+n <- 10          # Samples per Replication
+
+sigma <- 1       # Normal StdDev
+
+
+samples <- array(0, c(R, n))
+for (i in 1:R){
+  samples[i, ] <- rnorm(n, 0, sigma)    # Can we be more efficient without rewriting everything?
+}
+
+samples_stat <- array(0, R)
+samples_stat <- apply(samples, 1, var_b)
+
+
+par(mfrow=c(1,1), oma=c(0,0,0,0))
+
+hist(samples_stat, breaks= 40, probability = TRUE,    # Biased sample-variance (hist)
+    xlab=expression(sb^2), main= bquote(sb^2), cex.main=1.5, ylim = c(0, 1.5))
+
+# Biased sample-variance (line)
 curve(((n)/sigma^2) * dchisq(x * ((n)/sigma^2), df = n - 1),
+      add = TRUE, col="red", lwd=2, main="N(0,1)")
+
+# UN-biased sample-variance (line)
+curve(((n-1)/sigma^2) * dchisq(x * ((n-1)/sigma^2), df = n - 1),
       add = TRUE, col="blue", lwd=2, main="N(0,1)")
+
+# Make the "bias" more apparent
+segments(1, 0, 1, 1.2,
+         col="orange", lwd=2)
+
